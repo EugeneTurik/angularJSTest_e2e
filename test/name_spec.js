0a55b2d564e608d31baace4f4ee3c105ba842cc7
EugeneTurik <assamok@mail.ru>
2016-12-08 13:34:17 +0300

    PageObject Name

--- test/name_spec.js
+++ test/name_spec.js
@@ -1,37 +1,37 @@
+var NamePage = require ('/pages/name.page.js');
+
 describe('angularjs homepage Name', function() {
+
+    var namePage = new NamePage();
+
+    beforeEach(function() {
+       namePage.get();
+    });
+
     it('should greet the named Julie', function() {
-        browser.get('http://www.angularjs.org');
-        element(by.model('yourName')).sendKeys('Julie');
-        var greeting = element(by.binding('yourName'));
-        expect(greeting.getText()).toEqual('Hello Julie!');
+       namePage.fillName('Julie');
+        expect(namePage.getName()).toEqual('Hello Julie!');
     });
 
     it('should greet the named !@#$%^&*()', function() {
-        browser.get('http://www.angularjs.org');
-        element(by.model('yourName')).sendKeys('!@#$%^&*()');
-        var greeting = element(by.binding('yourName'));
-        expect(greeting.getText()).toEqual('Hello !@#$%^&*()!');
+        namePage.fillName('!@#$%^&*()');
+        expect(namePage.getName()).toEqual('Hello !@#$%^&*()!');
     });
 
     it('should greet the named Julie123', function() {
-        browser.get('http://www.angularjs.org');
-        element(by.model('yourName')).sendKeys('Julie123');
-        var greeting = element(by.binding('yourName'));
-        expect(greeting.getText()).toEqual('Hello Julie123!');
+        namePage.fillName('Julie123');
+        expect(namePage.getName()).toEqual('Hello Julie123!');
     });
 
     it('should greet the named Julie+100=3', function() {
-        browser.get('http://www.angularjs.org');
-        element(by.model('yourName')).sendKeys('Julie+100=3');
-        var greeting = element(by.binding('yourName'));
-        expect(greeting.getText()).toEqual('Hello Julie+100=3');
+        namePage.fillName('Julie+100=3');
+        expect(namePage.getName()).toEqual('Hello Julie+100=3');
     });
 
     it('clear the name Jon', function(){
-        browser.get('https://angularjs.org/');
-        element(by.model('youName')).sendKeys('Jon');
-        element(by.model('youName')).clear();
-        var greeting = element(by.binding('yourName'));
-        expect(greeting.getText()).toEqual();
-     });
+        namePage.fillName('Jon');
+        namePage.clearName();
+        expect(namePage.getName()).toEqual();
+    });
+
   });
