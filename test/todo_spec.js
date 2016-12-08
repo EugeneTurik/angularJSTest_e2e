c057318bfee3fb840547d3cb2795f3bb9843922a
EugeneTurik <assamok@mail.ru>
2016-12-08 13:50:02 +0300

    PageObject Todo

--- test/todo_spec.js
+++ test/todo_spec.js
@@ -1,34 +1,32 @@
+var TodoPage = require ('/pages/todo.page.js');
+
 describe('angularjs homepage todo list', function() {
-    it('should add a todo', function() {
-        browser.get('https://angularjs.org');
-        element(by.model('todoList.todoText')).sendKeys('first protractor test');
-        element(by.css('[value="add"]')).click();
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.get(2).getText()).toEqual('first protractor test');
+
+    var todoPage = new TodoPage;
+
+    beforeEach(function() {
+        todoPage.get();
     });
+
     it('should add a todo', function() {
-        browser.get('https://angularjs.org');
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.get(1).getText()).toEqual('build an angular app');
+        todoPage.fillTextSend('first protractor test');
+        todoPage.submitAdd;
+        expect(todoPage.todoListGet(2).getText()).toEqual('first protractor test');
     });
+
     it('should add a todo', function() {
-        browser.get('https://angularjs.org');
-        element(by.model('todoList.todoText')).sendKeys('first protractor test');
-        element(by.css('[value="add"]')).click();
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.count()).toEqual(3);
+        expect(todoPage.todoListGet(1).getText()).toEqual('build an angular app');
     });
+
+    it('should add a todo', function() {
+        todoPage.fillTextSend('first protractor test');
+        todoPage.submitAdd;
+        expect(todoPage.todoList.count()).toEqual(3);
+    });
+
     it('get element by todo', function() {
-        browser.get('https://angularjs.org');
-        element(by.model('todo.done')).click();
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        expect(todoList.get(1).getText()).toEqual('learn angular');
+        todoPage.todoCheckBoxClick;
+        expect(todoPage.todoListGet(1).getText()).toEqual('learn angular');
     });
-    it('element add a todo', function() {
-        browser.get('https://angularjs.org');
-        var todoList = element.all(by.repeater('todo in todoList.todos'));
-        todoList.get(2).element(by.css('input')).click();
-        var completedAmount = element.all(by.css('.done-true'));
-        expect(completedAmount.count()).toEqual(2);
-    });
+
 });
